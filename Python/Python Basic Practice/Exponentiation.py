6a75a288502edfd9f40810c1f2573136c46c10b7
Quoc Truong Ngo <72320787+ngoquoc-git@users.noreply.github.com>
2021-06-18 22:28:34 -0700

    Delete Python

--- Python/Python Basic Practice/Exponentiation.py
+++
@@ -1,5 +0,0 @@
-#You are offered a choice of either $1.000.000 or $0.01 (one penny) doubled every day for 30 days (the resulting amount is doubled every day). 
-#Write a program to calculate the amount that will result from the doubling to understand which choice results in a larger amount. 
-
-
-print("Doubling $0.01 after 30 day: ", 0.01*(2**30))
